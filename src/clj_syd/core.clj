8e90d73941ddd561a964102404263115b101cee7
Filippo Vitale <filippo@gruden.com>
2013-02-28 20:49:36 +1100

    implemented the stations generation (it works up to k=20)

--- src/clj_syd/core.clj
+++ src/clj_syd/core.clj
@@ -1,10 +1,14 @@
-(ns clj-syd.core
-  (:gen-class ))
+(ns clj-syd.core)
+
+(import java.math.BigInteger)
 
 (defn generate-stations
   "Generate the stations for n"
   [n]
-  (range 1 n)) ; TODO
+  (set (map #(list
+          (.modPow (biginteger 2) (biginteger %) (biginteger n))
+          (.modPow (biginteger 3) (biginteger %) (biginteger n)))
+    (range 0 (inc (* 2 n))))))
 
 (defn -main
   "Project E*l*r - Problem 411"
