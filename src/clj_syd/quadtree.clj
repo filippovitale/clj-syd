1a891c9397854ee396331211c6d688b6554325da
Filippo Vitale <filippo.vitale@gmail.com>
2013-03-27 08:22:57 +1100

    started teh quadtree

---
+++ src/clj_syd/quadtree.clj
@@ -0,0 +1,13 @@
+(defrecord tn [v l r])
+
+(defn quadtree
+  [minX minY maxX maxY]
+  (println "TODO new Node:" minX minY (- maxX minX) (- maxY minY)) ;TODO
+  )
+
+;count <--  number of items in the tree
+
+
+
+(quadtree 0 0 22 22)
+
