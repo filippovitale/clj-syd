7a45870c62f1aec8a718c48ee7dc832bc48d11cc
Filippo Vitale <filippo.vitale@gmail.com>
2013-04-03 15:45:36 +1100

    added TODOs

--- src/clj_syd/core.clj
+++ src/clj_syd/core.clj
@@ -21,11 +21,15 @@
   "Generate the DISTINCT stations for n"
   [n]
   (reduce
-    #(if (q/contain-station? %1 %2) (reduced %1) (q/insert-station %1 %2))
-      #{} ; TODO empty QuadTree
+    #(if (q/contain-station? %1 %2) (reduced %1) (q/insert-station %1 %2)) ; TODO instead of q/insert-station --> conj
+      #{} ; TODO empty QuadTree q/empty
     (station-generator-with-duplicates n)))
 ;http://clojure.github.com/clojure/clojure.core-api.html#clojure.core/reduced
 ;https://news.ycombinator.com/item?id=5304949
+
+;Try deftype and/or defrecord
+;http://stackoverflow.com/questions/2944108/implementing-custom-data-structures-using-clojure-protocols
+;http://techbehindtech.com/2010/11/15/clojure-defrecord-deftype/
 
 ;(station-generator 22)
 ;=>  #{[16 15] [1 1] [2 3] [4 9] [6 15] [20 3] [12 1] [10 1] [14 5] [18 9] [8 5]}
