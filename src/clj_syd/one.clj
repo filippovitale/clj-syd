de6add8a831e8f172a0cc6f760f328c2733075fe
Filippo Vitale <filippo.vitale@gmail.com>
2013-04-15 14:19:34 +1000

    wip

--- src/clj_syd/one.clj
+++ src/clj_syd/one.clj
@@ -23,3 +23,32 @@
         (into ()
           (take 13 (station-generator n)) ;clojure.lang.LazySeq
         )))))
+
+
+;(defn vrange [n]
+;  (loop [i 0 v []]
+;    (if (< i n)
+;      (recur (inc i) (conj v i))
+;      v)))
+;
+;(defn vrange2 [n]
+;  (loop [i 0 v (transient [])]
+;    (if (< i n)
+;      (recur (inc i) (conj! v i))
+;      (persistent! v))))
+;
+;(time (def v (vrange 1000000)))
+;"Elapsed time: 297.444 msecs"
+;
+;(time (def v2 (vrange2 1000000)))
+;"Elapsed time: 34.428 msecs"
+
+;(defn into
+;  "Returns a new coll consisting of to-coll with all of the items of
+;  from-coll conjoined."
+;  {:added "1.0"
+;   :static true}
+;  [to from]
+;  (if (instance? clojure.lang.IEditableCollection to)
+;    (with-meta (persistent! (reduce conj! (transient to) from)) (meta to))
+;    (reduce conj to from)))
