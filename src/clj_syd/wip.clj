e8befc5f74b90181c2bc72d32434253e6cbef57c
Filippo Vitale <filippo.vitale@gmail.com>
2013-04-20 09:30:33 +1000

    housekeeping

--- src/clj_syd/wip.clj
+++ src/clj_syd/wip.clj
@@ -1,4 +1,4 @@
-(ns clj-syd.one)
+(ns clj-syd.wip)
 
 (defn station-generator
   "Infinite Station Generator"
@@ -18,11 +18,19 @@
           (if-not (get-in a [x y])
             (update-in a [x] conj y)
             (reduced a))) ;)
+
+        ; TODO don't know why it is not working
+        ;(update-in (sorted-map) [1] conj (int 1))
+        ;(update-in (sorted-map) [1] conj 1)
+        ;(into (sorted-map) {1 (sorted-set 1)})
+        ;(drop 1 list-of-xy))))
+
         (sorted-map)
         list-of-xy))))
 
 (defn update-right
   [coll y]
+  ; TODO find a more efficient (*BISECT*) way to -> (count (filter #(>= y %) in))
   (let [i (count (filter #(>= y %) coll))]
     (assoc-in coll [i] y)
     ))
@@ -32,17 +40,18 @@
   (count
     (reduce
       update-right
-      (vector-of :int )
-      (reduce into (vector-of :int )
+      (vector-of :int ) ; same as []
+      (reduce into [] ; TODO maybe double cycle?
         (vals
           (station-structure-using-reduced
             (station-generator n)))))))
 
 (defn -main [& args]
   (let
-    [k (Integer. ^String (or (first args) "7"))
+    [k (Integer. ^String (or (first args) "2"))
      k->n (fn [k] (* k k k k k))
      n (k->n k)]
+    ;s (into () (take (* 2 n) (station-generator n)))]
     (time
       (printf "k=%s n=%d stations-x=%d\n" k n
         (solve n)))))
