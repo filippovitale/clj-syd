fb72cde12ee9d5e53f2abd84861dd1a0f60b6dfb
Filippo Vitale <filippo.vitale@gmail.com>
2013-09-23 19:57:24 +1000

    configured project.clj

--- hack-night-2-cljs-sourcemap/README.md
+++ hack-night-2-cljs-sourcemap/README.md
@@ -5,6 +5,24 @@ clj-syd Hack night code
 ## Steps
 * ```lein new modern-cljs```
 * Ctrl-Shift-N in explorer
+* update the ```project.clj``` with:
+```
+  :source-paths ["src/clj"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-1889"]]
+
+  :plugins [[lein-cljsbuild "0.3.3"]]
+
+  :cljsbuild 
+  {:builds
+    [{:id "advanced"
+      :source-paths ["src/cljs"]
+      :compiler {:optimizations :advanced
+                :pretty-print false
+                :output-dir "resources/public/js"
+                :output-to "modern.js"
+                :source-map "modern.js.map"}}]})
+```
 
 ## License
 
--- hack-night-2-cljs-sourcemap/modern-cljs/project.clj
+++ hack-night-2-cljs-sourcemap/modern-cljs/project.clj
@@ -1,6 +1,21 @@
 (defproject modern-cljs "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "clj-syd hack-night 26/09/2013"
+  :url "https://github.com/filippovitale/clj-syd"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+
+  :source-paths ["src/clj"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-1889"]]
+
+  :plugins [[lein-cljsbuild "0.3.3"]]
+
+  :cljsbuild 
+  {:builds
+    [{:id "advanced"
+      :source-paths ["src/cljs"]
+      :compiler {:optimizations :advanced
+                :pretty-print false
+                :output-dir "resources/public/js"
+                :output-to "modern.js"
+                :source-map "modern.js.map"}}]})
